d65cdb36d70cdd5058f34c41990b7774b2460989
BR <bjorn.roberg@gmail.com>
2026-03-19 01:30:12 +0100

    wander

---
+++ site/public/wander/wander.js
@@ -0,0 +1,12 @@
+window.wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+      'https://bjro.dev/wander/',
+  ],
+  // Websites and pages I recommend to other wanderers.
+  pages: [
+      'https://bjro.dev/',
+      'https://susam.net/',
+      'https://dahlstrand.net/wander/',
+  ],
+}
